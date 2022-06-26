dbf06c8c8600b6355508b290d40b0c2e6adbc0a7
BlueSpinney <99137204+BlueSpinney@users.noreply.github.com>
2022-06-26 14:40:50 +0200

    v2

--- calculator/index.js
+++ calculator/index.js
@@ -2,30 +2,64 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-
-
+let second = 0;
+let gop = ""
 let dict = {
+  0 : [],
+  1 : []
 }
-for (let i = 0; i < 9;i++){
-  dict[i] = null
-}
+
 
 
 class Button extends React.Component {
   render() {
     return (
-      <button onClick={this.props.OnClick}>{this.props.val}</button>
+      <button onClick={ () => this.props.OnClick(this.props.val)}>{this.props.val}</button>
+    );
+  }
+}
+
+class Calc extends React.Component {
+  render() {
+    return (
+      <button onClick={ () => this.props.OnClick()}>calculate</button>
+    );
+  }
+}
+
+class Operator extends React.Component {
+  render() {
+    return (
+      <button onClick={ () => this.props.pickop(this.props.op)}>{this.props.op}</button>
     );
   }
 }
   
   class Base extends React.Component {
+    chop = (op) => {
+      second = 1
+      gop = op
+    }
 
+    createop = (op) => {
+      return (
+        <Operator
+          pickop = {this.chop}
+          op = {op}
+        />
+      );
+    }
 
-    onC = () => {
-      for (let i = 0; i < Object.keys(dict).length;i++){
-        alert(dict[i]);
-      }
+    onC = (value) => {
+      dict[second].push(value)
+
+    }
+    calc = () => {
+      let t = dict[0].join("") + " " + gop + " " + dict[1].join("")
+      alert(eval(t))
+      dict[0] = []
+      dict[1] = []
+      second = 0
     }
 
     createbut(val){
@@ -42,7 +76,26 @@ class Button extends React.Component {
         <div>
             <div>
               {this.createbut(1)}
+              {this.createbut(2)}
+              {this.createbut(3)}
+              {this.createop("+")}
             </div>
+            <div>
+              {this.createbut(4)}
+              {this.createbut(5)}
+              {this.createbut(6)}
+              {this.createop("-")}
+            </div>   
+            <div>
+              {this.createbut(7)}
+              {this.createbut(8)}
+              {this.createbut(9)}
+              {this.createop("*")}
+            </div>         
+            {<Calc
+              OnClick = {this.calc}
+            />}
+            {this.createop("/")}
         </div>
       );
     }
